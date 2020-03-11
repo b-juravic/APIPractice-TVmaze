1521e2d199388e55861892e73833bb07ae5695ff
Brittiany Juravic <britjuravic@Russ-iPhone.localdomain>
2020-03-11 11:32:16 -0700

    WIP

--- tvmaze.js
+++ tvmaze.js
@@ -17,18 +17,23 @@
         image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
       }
  */
+
+
+// retrieves the user input value from search box
+
+async function userSearchRequestVal() {
+  let searchRequestVal = $("#search-query").val();
+  await searchShows(searchRequestVal);
+}
+
+// Searches show by name TVMaze API and returns array of objects
+
 async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
+  let showByNameUrl = "http://api.tvmaze.com/search/shows";
+  let returnedShow = await axios.get(showByNameUrl, {params: {q: query}});
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  return returnedShow;
 }
 
 
